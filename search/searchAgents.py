b5dc2fc62d48d7f45a9190de9bd10de425400a9d
jirsch <jirsch@yahoo-inc.com>
2014-11-19 00:30:10 +0200

    Suboptmial

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -483,70 +483,70 @@ def foodHeuristic(state, problem):
   position, foodGrid = state
   "*** YOUR CODE HERE ***"
   return 0
-  
-class ClosestDotSearchAgent(SearchAgent):
-  "Search for all food using a sequence of searches"
-  def registerInitialState(self, state):
-    self.actions = []
-    currentState = state
-    while(currentState.getFood().count() > 0): 
-      nextPathSegment = self.findPathToClosestDot(currentState) # The missing piece
-      self.actions += nextPathSegment
-      for action in nextPathSegment: 
-        legal = currentState.getLegalActions()
-        if action not in legal: 
-          t = (str(action), str(currentState))
-          raise Exception('findPathToClosestDot returned an illegal move: %s!\n%s' % t)
-        currentState = currentState.generateSuccessor(0, action)
-    self.actionIndex = 0
-    print('Path found with cost %d.' % len(self.actions))
-    
-  def findPathToClosestDot(self, gameState):
-    "Returns a path (a list of actions) to the closest dot, starting from gameState"
-    # Here are some useful elements of the startState
-    startPosition = gameState.getPacmanPosition()
-    food = gameState.getFood()
-    walls = gameState.getWalls()
-    problem = AnyFoodSearchProblem(gameState)
 
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-  
-class AnyFoodSearchProblem(PositionSearchProblem):
-  """
-    A search problem for finding a path to any food.
+
+class ClosestDotSearchAgent(SearchAgent):
+    "Search for all food using a sequence of searches"
+    def registerInitialState(self, state):
+        self.actions = []
+        currentState = state
+        while(currentState.getFood().count() > 0):
+            nextPathSegment = self.findPathToClosestDot(currentState) # The missing piece
+            self.actions += nextPathSegment
+            for action in nextPathSegment:
+                legal = currentState.getLegalActions()
+                if action not in legal:
+                    t = (str(action), str(currentState))
+                    raise Exception('findPathToClosestDot returned an illegal move: %s!\n%s' % t)
+                currentState = currentState.generateSuccessor(0, action)
+        self.actionIndex = 0
+        print('Path found with cost %d.' % len(self.actions))
     
+    def findPathToClosestDot(self, gameState):
+        "Returns a path (a list of actions) to the closest dot, starting from gameState"
+        # Here are some useful elements of the startState
+        startPosition = gameState.getPacmanPosition()
+        food = gameState.getFood()
+        walls = gameState.getWalls()
+        problem = AnyFoodSearchProblem(gameState)
+
+        return search.breadthFirstSearch(problem)
+
+
+class AnyFoodSearchProblem(PositionSearchProblem):
+    """
+    A search problem for finding a path to any food.
+
     This search problem is just like the PositionSearchProblem, but
     has a different goal test, which you need to fill in below.  The
     state space and successor function do not need to be changed.
-    
+
     The class definition above, AnyFoodSearchProblem(PositionSearchProblem),
     inherits the methods of the PositionSearchProblem.
-    
-    You can use this search problem to help you fill in 
+
+    You can use this search problem to help you fill in
     the findPathToClosestDot method.
-  """
-
-  def __init__(self, gameState):
-    "Stores information from the gameState.  You don't need to change this."
-    # Store the food for later reference
-    self.food = gameState.getFood()
-
-    # Store info for the PositionSearchProblem (no need to change this)
-    self.walls = gameState.getWalls()
-    self.startState = gameState.getPacmanPosition()
-    self.costFn = lambda x: 1
-    self._visited, self._visitedlist, self._expanded = {}, [], 0
-    
-  def isGoalState(self, state):
     """
-    The state is Pacman's position. Fill this in with a goal test
-    that will complete the problem definition.
-    """
-    x,y = state
-    
-    "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    def __init__(self, gameState):
+        "Stores information from the gameState.  You don't need to change this."
+        # Store the food for later reference
+        self.food = gameState.getFood()
+
+        # Store info for the PositionSearchProblem (no need to change this)
+        self.walls = gameState.getWalls()
+        self.startState = gameState.getPacmanPosition()
+        self.costFn = lambda x: 1
+        self._visited, self._visitedlist, self._expanded = {}, [], 0
+
+    def isGoalState(self, state):
+        """
+        The state is Pacman's position. Fill this in with a goal test
+        that will complete the problem definition.
+        """
+        x, y = state
+        return self.food[x][y]
+
 
 ##################
 # Mini-contest 1 #
